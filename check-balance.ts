dbbc18464f92edc6097edfd954b1cd986d6883f3
Leta Sonce <letasonce@MacBook-Pro-Leta.local>
2025-02-18 18:36:42 -0800

    fix: add publicKey in if

--- check-balance.ts
+++ check-balance.ts
@@ -2,13 +2,17 @@ import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from '@solana/
 
 // for to check balances on any wallet
 const suppliedPublicKey = process.argv[2];
+let publicKey;
 if (!suppliedPublicKey) {
-  throw new Error("Provide a public key to check the balance of!");
+  publicKey = new PublicKey('48YJZcq15botTTXNkv2LLWewyZLxb6z9aEjYNaH3BkSn');
+  //throw new Error("Provide a public key to check the balance of!");
+} else {
+  publicKey = new PublicKey(suppliedPublicKey);
 }
 
 
 //const publicKey = new PublicKey("48YJZcq15botTTXNkv2LLWewyZLxb6z9aEjYNaH3BkSn");
-const publicKey = new PublicKey(suppliedPublicKey);
+
 //const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 const connection = new Connection(clusterApiUrl("devnet"));
 const balanceInLamports = await connection.getBalance(publicKey);
